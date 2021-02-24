a58436c120b62ae7632277968605265f64ebe8bc
david-shakaya <devidoz666@gmail.com>
2021-02-24 20:45:22 +0200

    + черновое меню для телефона

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './sass/main.scss';
+
+const body = document.querySelector('.body');
+const btnMobileMenu = document.querySelector('.menu-btn-tel');
+const menuWrapper = document.querySelector('.menu-wrapper-tel');
+const btnCloseMobileMenu = document.querySelector('.close-mobile-menu');
+
+btnMobileMenu.addEventListener('click', handlleShowMenu);
+btnCloseMobileMenu.addEventListener('click', handlleHideMenu);
+
+function handlleShowMenu() {
+  menuWrapper.classList.add('active');
+  body.classList.add('active');
+}
+
+function handlleHideMenu() {
+  menuWrapper.classList.remove('active');
+  body.classList.remove('active');
+}
